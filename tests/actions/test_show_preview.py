51b8625227bd02f208f19490374882b50f40d0ad
Edvaldo José <edvaldojose003@gmail.com>
2023-07-06 11:26:56 -0300

    feat: correction of requirement 3

--- tests/actions/test_show_preview.py
+++ tests/actions/test_show_preview.py
@@ -1,33 +1,46 @@
 from pro_filer.actions.main_actions import show_preview  # NOQA
 
 
-def test_show_preview_with_diles_and_dirs(capsys):
+def test_show_preview_with_files_and_dirs(capsys):
     context = {
         "all_files":
-            ["file1.txt", "file2.txt", "file3.txt", "file4.txt", "file5.txt"],
-        "all_dirs": ["dir1", "dir2", "dir3", "dir4", "dir5"]
+            ["src/__init__.py", "src/app.py", "src/utils/__init__.py"],
+        "all_dirs": ["src", "src/utils"]
     }
 
     show_preview(context)
 
     captured = capsys.readouterr()
 
-    expected_output = "Found 5 files and 5 directories\n"
-    expected_output += "First 5 files: ['file1.txt', 'file2.txt', " \
-                        "'file3.txt', 'file4.txt', 'file5.txt']\n"
-    expected_output += "First 5 directories: ['dir1', 'dir2', 'dir3', " \
-                        "'dir4', 'dir5']\n"
+    expected_output = (
+        "Found 3 files and 2 directories\n"
+        "First 5 files: ['src/__init__.py', 'src/app.py', "
+                        "'src/utils/__init__.py']\n"
+        "First 5 directories: ['src', 'src/utils']\n"
+    )
+
     assert captured.out == expected_output
 
 
 def test_show_preview_without_files_or_dirs(capsys):
     context = {
-        "all_files": [],
-        "all_dirs": []
+        "all_files":
+            ["file1.txt", "file2.txt",
+                "file3.txt", "file4.txt", "file5.txt", "file6.txt"],
+        "all_dirs":
+            ["dir1", "dir2", "dir3", "dir4", "dir5", "dir6"]
     }
 
     show_preview(context)
 
     captured = capsys.readouterr()
-    expected_output = "Found 0 files and 0 directories\n"
+
+    expected_output = (
+        "Found 6 files and 6 directories\n"
+        "First 5 files: ['file1.txt', 'file2.txt', "
+                        "'file3.txt', 'file4.txt', 'file5.txt']\n"
+        "First 5 directories: ['dir1', 'dir2', 'dir3', "
+                        "'dir4', 'dir5']\n"
+    )
+
     assert captured.out == expected_output
